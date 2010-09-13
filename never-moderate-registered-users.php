25b2eae74e5c2a67a3a367b0be9b011647d39b11
Scott Reilly <scott@coffee2code.com>
2010-09-13 14:52:11 -0400

    Release v2.0:
    * Add filter 'c2c_never_moderate_registered_users_caps' to allow specifying capabilities a user must have in order to bypass moderation. If none are specified, then the user just has to be registered.
    * Remove $min_user_level argument and support
    * Wrap function in if(!function_exists()) check
    * Remove docs from top of plugin file (all that and more are in readme.txt)
    * Minor code improvements and reformatting (spacing)
    * Add PHPDoc
    * Note compatibility with WP 3.0+
    * Drop compatibility with versions of WP older than 2.8
    * Update copyright date
    * Add package info to top of plugin file
    * Add Changelog, Filters, and Upgrade Notice sections to readme.txt
    * Add to plugin repository

--- never-moderate-registered-users.php
+++ never-moderate-registered-users.php
@@ -1,51 +1,31 @@
 <?php
+/**
+ * @package Never_Moderate_Registered_Users
+ * @author Scott Reilly
+ * @version 2.0
+ */
 /*
 Plugin Name: Never Moderate Registered Users
-Version: 1.0
-Plugin URI: http://coffee2code.com/wp-plugins/never-moderate-registered-users
+Version: 2.0
+Plugin URI: http://coffee2code.com/wp-plugins/never-moderate-registered-users/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Never moderate or mark as spam comments made by registered users, regardless of the apparent spamminess of the comment.
 
-To be recognized as a registered user, the user must be logged into your blog at the time they post their comment.
+Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+.
 
-This plugin assumes that you trust your registered users.  It will automatically approve any comment made by
-registered users, even if the comment stinks of spam.  Therefore, it is recommended that you do not allow users
-to register themselves (uncheck the setting "Anyone can register" in the WordPress admin under Options -> General,
-or in WP 2.5: Settings -> General).
-
-For those wanting to allow people to register themselves, and still have those people (called "subscribers" by
-WordPress) to be moderated as necessary, but allow for other higher-level trusted users from being moderated, you
-can still do so.  Edit the plugin source file and change the $min_user_level argument of 
-	function c2c_never_moderate_registered_users( $approved, $min_user_level='0' )
-to a higher value than "0".  "1" corresponds to a "contributor".
-See http://codex.wordpress.org/Roles_and_Capabilities#level_10 for more info on user levels and roles.
-
-This plugin is a partial successor to my now-defunct Never Moderate Admins or Post Author.  In addition to
-preventing admins and the post's author from being moderated, that plugin also allowed you to prevent registered 
-users from being moderated.  WordPress has since integrated that functionality, so the main thrust of that plugin
-became moot.  However, the ability to never moderate registered users is still a valid need that requires this new
-plugin.
-
-Compatible with WordPress 2.3+, and 2.5+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/never-moderate-registered-users.zip and unzip it into your 
-/wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/never-moderate-registered-users/
 
 */
 
 /*
-Copyright (c) 2008 by Scott Reilly (aka coffee2code)
+Copyright (c) 2008-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -57,21 +37,45 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
-// All users with a user level greater than or equal to $min_user_level will NEVER be moderated
-function c2c_never_moderate_registered_users( $approved, $min_user_level='0' ) {
+/**
+ * Never moderate comments by registered users.
+ *
+ * @since 1.0
+ *
+ * @param int $approved Current approval status for comment
+ * @return int If the comment is approved
+ */
+if ( !function_exists( 'c2c_never_moderate_registered_users' ) ) :
+function c2c_never_moderate_registered_users( $approved ) {
 	global $wpdb, $commentdata;
-	$user_id = $commentdata['user_ID'];
-	// If the comment isn't from a registered user, don't change approval status
-	if ( !$user_id )
+	$user_id = isset( $commentdata['user_ID'] ) ? $commentdata['user_ID'] : false;
+
+	// If the comment isn't from a registered user, or is already approved, don't change approval status
+	if ( !$user_id || $approved )
 		return $approved;
 
- 	$user = new WP_User($user_id);
-	if ( $user && (1 !== $approved) && $user->has_cap('level_' . $min_user_level) )
-		$approved = 1;
+	$user = new WP_User( $user_id );
+	if ( $user ) {
+		$trusted_caps = (array) apply_filters( 'c2c_never_moderate_registered_users_caps', array() );
+		if ( empty( $trusted_caps ) ) {
+			$has_cap = true;
+		} else {
+			$has_cap = false;
+			foreach ( $trusted_caps as $cap ) {
+				if ( $user->has_cap( $cap ) ) {
+					$has_cap = true;
+					break;
+				}
+			}
+		}
+		if ( $has_cap )
+			$approved = 1;
+	}
 
 	return $approved;
 }
+endif;
 
-add_filter('pre_comment_approved', 'c2c_never_moderate_registered_users', 15);
+add_filter( 'pre_comment_approved', 'c2c_never_moderate_registered_users', 15 );
 
 ?>
